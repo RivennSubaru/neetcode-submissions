c174fd459150d0ef7b74679b3a5b5366c1bfb4a0
Steven <135208169+RivennSubaru@users.noreply.github.com>
2026-06-23 19:04:12 +0300

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        hs, ht = {}, {}
+        for i in range(len(s)):
+            hs[s[i]] = 1 if s[i] not in hs else hs[s[i]] + 1
+            ht[t[i]] = 1 if t[i] not in ht else ht[t[i]] + 1
+        return hs == ht
